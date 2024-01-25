a6384dd0bb928bbb693ffb64bd3d95293f0598ad
Muhammad Ehtesham <muhammadehtesham69@gmail.com>
2024-01-25 21:15:06 +0500

    first commit

--- controller/user.js
+++ controller/user.js
@@ -247,7 +247,7 @@ router.put(
             });
         } catch (error) {
             return next(new ErrorHandler(error.message, 500));
-        } 
+        }
     })
 );
 
@@ -256,24 +256,59 @@ router.delete(
     "/delete-user-address/:id",
     isAuthenticated,
     catchAsyncErrors(async (req, res, next) => {
-      try {
-        const userId = req.user._id;
-        const addressId = req.params.id;
-  
-        await User.updateOne(
-          {
-            _id: userId,
-          },
-          { $pull: { addresses: { _id: addressId } } }
-        ); 
-    
-        const user = await User.findById(userId);
-  
-        res.status(200).json({ success: true, user });
-      } catch (error) {
-        return next(new ErrorHandler(error.message, 500));
-      }
+        try {
+            const userId = req.user._id;
+            const addressId = req.params.id;
+
+            await User.updateOne(
+                {
+                    _id: userId,
+                },
+                { $pull: { addresses: { _id: addressId } } }
+            );
+
+            const user = await User.findById(userId);
+
+            res.status(200).json({ success: true, user });
+        } catch (error) {
+            return next(new ErrorHandler(error.message, 500));
+        }
     })
-  );
+);
+
+// update user password
+router.put(
+    "/update-user-password",
+    isAuthenticated,
+    catchAsyncErrors(async (req, res, next) => {
+        try {
+            const user = await User.findById(req.user.id).select("+password");
+
+            const isPasswordMatched = await user.comparePassword(
+                req.body.oldPassword
+            );
+
+            if (!isPasswordMatched) {
+                return next(new ErrorHandler("Old password is incorrect!", 400));
+            }
+
+            if (req.body.newPassword !== req.body.confirmPassword) {
+                return next(
+                    new ErrorHandler("Password doesn't matched with each other!", 400)
+                );
+            }
+            user.password = req.body.newPassword;
+
+            await user.save();
+
+            res.status(200).json({
+                success: true,
+                message: "Password updated successfully!",
+            });
+        } catch (error) {
+            return next(new ErrorHandler(error.message, 500));
+        }
+    })
+);
 
 module.exports = router; 
