a781bbf30ced46db7cff66b1bea1e80801a99b50
Muhammad Ehtesham <muhammadehtesham69@gmail.com>
2024-01-28 16:06:19 +0500

    first commit

--- controller/payment.js
+++ controller/payment.js
@@ -21,5 +21,12 @@ router.post(
     })
 );
 
+router.get(
+    "/stripeapikey",
+    catchAsyncErrors(async (req, res, next) => {
+        res.status(200).json({ stripeApikey: process.env.STRIPE_API_KEY });
+    })
+);
+
 
 module.exports = router;
