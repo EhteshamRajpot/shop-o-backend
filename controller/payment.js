1fd1047c9c6fa0402e88d5256289d8993c59ac5c
Muhammad Ehtesham <muhammadehtesham69@gmail.com>
2024-01-28 21:02:40 +0500

    first commit

--- controller/payment.js
+++ controller/payment.js
@@ -9,7 +9,7 @@ router.post(
     catchAsyncErrors(async (req, res, next) => {
         const myPayment = await stripe.paymentIntents.create({
             amount: req.body.amount,
-            currency: "USD",
+            currency: "inr",
             metadata: {
                 company: "Becodemy",
             },
