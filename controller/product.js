f43bb99d8e83005813a698e6a9ddbe1fe56156f8
Muhammad Ehtesham <muhammadehtesham69@gmail.com>
2024-01-29 22:20:34 +0500

    first commit

--- controller/product.js
+++ controller/product.js
@@ -2,11 +2,13 @@ const express = require("express");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const router = express.Router();
 const Product = require("../model/product");
+const Order = require("../model/order");
 const Shop = require("../model/shop");
 const { upload } = require("../multer");
 const ErrorHandler = require("../utils/ErrorHandler");
 const { isSeller } = require("../middleware/auth");
 const fs = require("fs")
+const {isAuthenticated } = require("../middleware/auth");
 
 // create product
 router.post("/create-product", upload.array("images"), catchAsyncErrors(async (req, res, next) => {
@@ -27,7 +29,7 @@ router.post("/create-product", upload.array("images"), catchAsyncErrors(async (r
             res.status(201).json({
                 success: true,
                 product
-            })
+            }) 
 
         }
     } catch (error) {
@@ -65,9 +67,9 @@ router.delete("/delete-shop-product/:id", isSeller, catchAsyncErrors(async (req,
 
             fs.unlink(filePath, (err) => {
                 if (err) {
-                    console.log(err) 
+                    console.log(err)
                 }
-            }) 
+            })
         })
 
         const product = await Product.findByIdAndDelete(productId);
@@ -87,20 +89,97 @@ router.delete("/delete-shop-product/:id", isSeller, catchAsyncErrors(async (req,
 }))
 
 // get all products
-router.get( 
+router.get(
     "/get-all-products",
     catchAsyncErrors(async (req, res, next) => {
-      try {
-        const products = await Product.find().sort({ createdAt: -1 });
-  
-        res.status(201).json({
-          success: true,
-          products,
-        });
-      } catch (error) {
-        return next(new ErrorHandler(error, 400));
-      }
+        try {
+            const products = await Product.find().sort({ createdAt: -1 });
+
+            res.status(201).json({
+                success: true,
+                products,
+            });
+        } catch (error) {
+            return next(new ErrorHandler(error, 400));
+        }
     })
-  );
+);
+
+// review for a product
+router.put(
+    "/create-new-review",
+    isAuthenticated,
+    catchAsyncErrors(async (req, res, next) => {
+        try {
+            const { user, rating, comment, productId, orderId } = req.body;
+
+            const product = await Product.findById(productId);
+
+            const review = {
+                user,
+                rating,
+                comment,
+                productId,
+            };
+
+            const isReviewed = product.reviews.find(
+                (rev) => rev.user._id === req.user._id
+            );
+
+            if (isReviewed) {
+                product.reviews.forEach((rev) => {
+                    if (rev.user._id === req.user._id) {
+                        (rev.rating = rating), (rev.comment = comment), (rev.user = user);
+                    }
+                });
+            } else {
+                product.reviews.push(review);
+            }
+
+            let avg = 0;
+
+            product.reviews.forEach((rev) => {
+                avg += rev.rating;
+            });
+
+            product.ratings = avg / product.reviews.length;
+
+            await product.save({ validateBeforeSave: false });
+
+            await Order.findByIdAndUpdate(
+                orderId,
+                { $set: { "cart.$[elem].isReviewed": true } },
+                { arrayFilters: [{ "elem._id": productId }], new: true }
+            );
+
+            res.status(200).json({
+                success: true,
+                message: "Reviwed succesfully!",
+            });
+        } catch (error) {
+            return next(new ErrorHandler(error, 400));
+        }
+    })
+);
+
+// all products --- for admin
+router.get(
+    "/admin-all-products",
+    // isAuthenticated,
+    // isAdmin("Admin"),
+    catchAsyncErrors(async (req, res, next) => {
+        try {
+            const products = await Product.find().sort({
+                createdAt: -1,
+            });
+            res.status(201).json({
+                success: true,
+                products,
+            });
+        } catch (error) {
+            return next(new ErrorHandler(error.message, 500));
+        }
+    })
+);
 
 module.exports = router;
--- model/product.js
+++ model/product.js
@@ -34,6 +34,29 @@ const productSchema = new mongoose.Schema({
             type: String
         }
     ],
+    reviews: [
+        {
+            user: {
+                type: Object,
+            },
+            rating: {
+                type: Number,
+            },
+            comment: {
+                type: String,
+            },
+            productId: {
+                type: String,
+            },
+            createdAt: {
+                type: Date,
+                default: Date.now(),
+            }
+        },
+    ],
+    ratings: {
+        type: Number,
+    },
     shopId: {
         type: String,
         required: true,
