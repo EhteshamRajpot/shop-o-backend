e69ebc75f52f581f48ed33207ff3e45d703bdb0c
Muhammad Ehtesham <muhammadehtesham69@gmail.com>
2024-01-30 23:41:31 +0500

    first commit

--- controller/shop.js
+++ controller/shop.js
@@ -178,4 +178,166 @@ router.get(
 );
 
 
+// update shop profile picture
+router.put(
+    "/update-shop-avatar",
+    isSeller,
+    catchAsyncErrors(async (req, res, next) => {
+        try {
+            let existsSeller = await Shop.findById(req.seller._id);
+
+            const imageId = existsSeller.avatar.public_id;
+
+            await cloudinary.v2.uploader.destroy(imageId);
+
+            const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+                folder: "avatars",
+                width: 150,
+            });
+
+            existsSeller.avatar = {
+                public_id: myCloud.public_id,
+                url: myCloud.secure_url,
+            };
+
+
+            await existsSeller.save();
+
+            res.status(200).json({
+                success: true,
+                seller: existsSeller,
+            });
+        } catch (error) {
+            return next(new ErrorHandler(error.message, 500));
+        }
+    })
+);
+
+// update seller info
+router.put(
+    "/update-seller-info",
+    isSeller,
+    catchAsyncErrors(async (req, res, next) => {
+        try {
+            const { name, description, address, phoneNumber, zipCode } = req.body;
+
+            const shop = await Shop.findOne(req.seller._id);
+
+            if (!shop) {
+                return next(new ErrorHandler("User not found", 400));
+            }
+
+            shop.name = name;
+            shop.description = description;
+            shop.address = address;
+            shop.phoneNumber = phoneNumber;
+            shop.zipCode = zipCode;
+
+            await shop.save();
+
+            res.status(201).json({
+                success: true,
+                shop,
+            });
+        } catch (error) {
+            return next(new ErrorHandler(error.message, 500));
+        }
+    })
+);
+
+// all sellers --- for admin
+router.get(
+    "/admin-all-sellers",
+    isAuthenticated,
+    // isAdmin("Admin"),
+    catchAsyncErrors(async (req, res, next) => {
+        try {
+            const sellers = await Shop.find().sort({
+                createdAt: -1,
+            });
+            res.status(201).json({
+                success: true,
+                sellers,
+            });
+        } catch (error) {
+            return next(new ErrorHandler(error.message, 500));
+        }
+    })
+);
+
+// delete seller ---admin
+router.delete(
+    "/delete-seller/:id",
+    isAuthenticated,
+    // isAdmin("Admin"),
+    catchAsyncErrors(async (req, res, next) => {
+        try {
+            const seller = await Shop.findById(req.params.id);
+
+            if (!seller) {
+                return next(
+                    new ErrorHandler("Seller is not available with this id", 400)
+                );
+            }
+
+            await Shop.findByIdAndDelete(req.params.id);
+
+            res.status(201).json({
+                success: true,
+                message: "Seller deleted successfully!",
+            });
+        } catch (error) {
+            return next(new ErrorHandler(error.message, 500));
+        }
+    })
+);
+
+// update seller withdraw methods --- sellers
+router.put(
+    "/update-payment-methods",
+    isSeller,
+    catchAsyncErrors(async (req, res, next) => {
+        try {
+            const { withdrawMethod } = req.body;
+
+            const seller = await Shop.findByIdAndUpdate(req.seller._id, {
+                withdrawMethod,
+            });
+
+            res.status(201).json({
+                success: true,
+                seller,
+            });
+        } catch (error) {
+            return next(new ErrorHandler(error.message, 500));
+        }
+    })
+);
+
+// delete seller withdraw merthods --- only seller
+router.delete(
+    "/delete-withdraw-method/",
+    isSeller,
+    catchAsyncErrors(async (req, res, next) => {
+        try {
+            const seller = await Shop.findById(req.seller._id);
+
+            if (!seller) {
+                return next(new ErrorHandler("Seller not found with this id", 400));
+            }
+
+            seller.withdrawMethod = null;
+
+            await seller.save();
+
+            res.status(201).json({
+                success: true,
+                seller,
+            });
+        } catch (error) {
+            return next(new ErrorHandler(error.message, 500));
+        }
+    })
+);
+
 module.exports = router;  
