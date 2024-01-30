3afa28154ed70574984e9515dd53957235148659
Muhammad Ehtesham <muhammadehtesham69@gmail.com>
2024-01-30 14:39:58 +0500

    first commit

--- controller/order.js
+++ controller/order.js
@@ -108,7 +108,7 @@ router.put(
                 order.cart.forEach(async (o) => {
                     await updateOrder(o._id, o.qty);
                 });
-            }
+            } 
 
             order.status = req.body.status;
 
