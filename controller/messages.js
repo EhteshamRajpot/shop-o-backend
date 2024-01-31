8bdcc9d8b24530b4fe7700eb2bad9bd9dbcba7c3
Muhammad Ehtesham <muhammadehtesham69@gmail.com>
2024-02-01 00:23:08 +0500

    first commit

--- controller/messages.js
+++ controller/messages.js
@@ -2,66 +2,56 @@ const Messages = require("../model/messages");
 const ErrorHandler = require("../utils/ErrorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const express = require("express");
-const cloudinary = require("cloudinary");
+const { upload } = require("../multer");
 const router = express.Router();
 
 // create new message
-router.post(
-  "/create-new-message",
-  catchAsyncErrors(async (req, res, next) => {
+router.post("/create-new-message", upload.array("images"), catchAsyncErrors(async (req, res, next) => {
     try {
-      const messageData = req.body;
+        const messageData = req.body;
+        if (req.files) {
+            const files = req.files;
+            const imageUrls = files.map((file) => `${file.filename}`)
+            messageData.images = imageUrls
+        }
 
-      if (req.body.images) {
-        const myCloud = await cloudinary.v2.uploader.upload(req.body.images, {
-          folder: "messages",
-        });
-        messageData.images = {
-          public_id: myCloud.public_id,
-          url: myCloud.url,
-        };
-      }
+        messageData.conversationId = req.body.conversationId;
+        messageData.sender = req.body.sender;
 
-      messageData.conversationId = req.body.conversationId;
-      messageData.sender = req.body.sender;
-      messageData.text = req.body.text;
+        const message = new Messages({
+            conversationId: messageData.conversationId,
+            sender: messageData.sender,
+            images: messageData.images ? messageData.images : undefined
+        })
 
-      const message = new Messages({
-        conversationId: messageData.conversationId,
-        text: messageData.text,
-        sender: messageData.sender,
-        images: messageData.images ? messageData.images : undefined,
-      });
+        await message.save()
 
-      await message.save();
-
-      res.status(201).json({
-        success: true,
-        message,
-      });
+        res.status(201).json({
+            success: true,
+            message
+        })
     } catch (error) {
-      return next(new ErrorHandler(error.message), 500);
+        return next(new ErrorHandler(error.message), 500);
     }
-  })
-);
+}))
 
 // get all messages with conversation id
 router.get(
-  "/get-all-messages/:id",
-  catchAsyncErrors(async (req, res, next) => {
-    try {
-      const messages = await Messages.find({
-        conversationId: req.params.id,
-      });
+    "/get-all-messages/:id",
+    catchAsyncErrors(async (req, res, next) => {
+        try {
+            const messages = await Messages.find({
+                conversationId: req.params.id,
+            });
 
-      res.status(201).json({
-        success: true,
-        messages,
-      });
-    } catch (error) {
-      return next(new ErrorHandler(error.message), 500);
-    }
-  })
+            res.status(201).json({
+                success: true,
+                messages,
+            });
+        } catch (error) {
+            return next(new ErrorHandler(error.message), 500);
+        }
+    })
 );
 
 module.exports = router;
